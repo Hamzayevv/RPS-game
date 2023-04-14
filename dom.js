40dd061f4c7314b19c9c596ae73d3c60cfaacb92
Hamzayevv <106085668+Hamzayevv@users.noreply.github.com>
2023-04-14 06:46:21 -0700

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,10 @@
+var playername = document.querySelector("#playername")
+
+var playerscoretag = document.querySelector("#playerscore")
+var playerimg = document.querySelector("#playerimg")
+var playerpartresult = document.querySelector("#playerpartresult")
+
+
+var botscoretag = document.querySelector("#botscore")
+var botimg = document.querySelector("#botimg")
+var botpartresult = document.querySelector("#botpartresult")
